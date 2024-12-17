d468ffa24596beb32eb074e20949fd13de78ffa8
darik <darik.ter@gmail.com>
2024-12-17 19:06:47 +0300

    присоединили бота

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from dotenv import load_dotenv
+from logging import basicConfig, DEBUG
+import os
+from asyncio import run
 from aiogram import Bot,Dispatcher
-API_KEY = '7043185547:AAGBVS_Z0LoAKQY8EshimvjGcTXmP-rh-UU'
 
+load_dotenv() #выгружаем переменные из env файла
+API_KEY = os.getenv('API_KEY')
 patcher_bot = Bot(API_KEY) #это бот, он умеет все что угодно, но не принимает запросы
 router = Dispatcher() #это диспатчер он не умеет ничего, но смотрит кто что делвет (управляющий)
+basicConfig(level = DEBUG) #включили логирование
+
+async def main():
+    try:
+        router.start_polling(patcher_bot)
+        await patcher_bot.delete_webhook(drop_pending_updates=True)
+    except Exception as E:
+        print(E)
+
+if __name__ == "__main__":
+    run(main())
